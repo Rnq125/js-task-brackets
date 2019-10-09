f72fe0a0ed4fc8edced99a0f193e56e3da20d11a
Sergej <karyuhin.sergej@gmail.com>
2019-10-09 21:08:20 +0300

    uploaded

---
+++ src/index.js
@@ -0,0 +1,27 @@
+module.exports = function check(str, bracketsConfig) {
+	let stack = [];
+
+	str.split('').forEach((item, i) => {
+
+		if(i == 0) {
+			stack.push(item);
+			return;
+		}
+
+		let lastOpen = bracketsConfig.find(type =>
+			stack[stack.length - 1] == type[0]);
+
+		lastOpen = lastOpen && lastOpen[1];
+
+		if(item != lastOpen){
+			stack.push(item);
+		}
+		else{
+			stack.pop();
+		}
+
+	});
+
+	if(stack.length == 0) return true;
+	return false;
+}
